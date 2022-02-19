7ebbff194ffff1511a301425488a9cf798f4d240
Käkäsh <marco.zillgen23@gmail.com>
2022-02-19 14:54:03 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const noteDiv = document.getElementById("note-container");
-const bookDiv = document.getElementById("book-container");
-const inputDiv = document.getElementById("input-container");
+const noteContainer = document.getElementById("note-container");
+const bookContainer = document.getElementById("book-container");
+const inputContainer = document.getElementById("input-container");
 
-function addNewBook() {
-    bookDiv.appendChild(document.createElement("div")).setAttribute("class","book-entry")
+let bookDiv = [];
+
+function bookToDiv() {
+    // ein neues div machen css-klasse book-entry und in bookDiv anhängen
+    bookDiv[bookDiv.length] = document.createElement("div")
+    bookDiv[bookDiv.length-1].setAttribute("class","book-entry")
+    bookDiv[bookDiv.length-1].setAttribute("id","bookDiv"+bookDiv.length)
+    bookContainer.appendChild(bookDiv[bookDiv-length-1])
 }
 
-addNewBook()
+bookToDiv()
